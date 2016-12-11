691f3611ed913b04c154a2103bd2434183d8dfde
Rasel <mdalrasel1@gmail.com>
2016-12-11 18:27:02 +0600

    Add project files.

---
+++ WebApplication2/Controllers/ProductsController.cs
@@ -0,0 +1,39 @@
+﻿using ProductsApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web.Http;
+
+namespace ProductsApp.Controllers
+{
+    [RoutePrefix("Rasel")]
+    public class ProductsController : ApiController
+    {
+        
+       static Product[] products = new Product[] 
+        { 
+            new Product { Id = 1, Name = "Tomato Soup", Category = "Groceries", Price = 1 }, 
+            new Product { Id = 2, Name = "Yo-yo", Category = "Toys", Price = 3.75M }, 
+            new Product { Id = 3, Name = "Hammer", Category = "Hardware", Price = 16.99M } 
+        };
+
+        [HttpPost,Route("p1")]
+        public String GetAllProducts([FromBody]String p)
+        {
+            return p;
+        }
+
+
+      
+        public IHttpActionResult GetProduct(int id)
+        {
+            var product = products.FirstOrDefault((p) => p.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return Ok(product);
+        }
+    }
+}
